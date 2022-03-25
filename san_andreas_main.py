6ae92cff2f1de8d3f94e84edf508ff2c94bc330c
SerLeopold <79343488+SerLeopold@users.noreply.github.com>
2022-03-26 00:04:17 +0100

    Add files via upload

---
+++ san_andreas_main.py
@@ -0,0 +1,10 @@
+# Made by SerLeopold. This is my first project that I made on my own in Python.
+#2022.03.25.
+
+
+
+print("How old is Grand Theft Auto: San Andreas?")
+game_year = 2004
+this_year = input("Type what year are we in: ")
+game_age = int(this_year)-game_year
+print("Grand Theft Auto San Andreas is" ,game_age, "years old." )
